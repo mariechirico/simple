8b54298b92a5fc52b81361570e571e1b2fb629b9
Marie Chirico <marie.chirico@gmail.com>
2020-11-14 14:13:16 -0500

    Re-initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function showList(){
+    var myList = document.getElementById("main-list");
+    
+    myList.style.display = "block";
+    myList.className = "main-list";
+    
+}
+function reset(){
+    var myList = document.getElementById("main-list");
+    myList.className = "fade";
+
+    setTimeout(()=> {myList.style.display = "none";}, 500);
+}
