22ca2f2b9652be18e0b7660496902c861c50927e
Marie Chirico <marie.chirico@gmail.com>
2020-11-15 11:11:40 -0500

    Menu icons

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-function showList(){
-    var myList = document.getElementById("main-list");
+function showList(id){
+    var arrow = id + '-arrow';
+    var myList = document.getElementById(id);
+    var myArrow = document.getElementById(arrow);
     
     myList.style.display = "block";
     myList.className = "main-list";
-    
+
+    myArrow.className = "fas fa-angle-up";
 }
-function reset(){
-    var myList = document.getElementById("main-list");
+function reset(id){
+    var arrow = id + '-arrow';
+    var myList = document.getElementById(id);
+    var myArrow = document.getElementById(arrow);
+
     myList.className = "fade"; 
+
+    myArrow.className = "fas fa-angle-down";
 
     setTimeout(()=> {myList.style.display = "none";}, 500); 
 }
