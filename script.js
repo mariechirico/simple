8b7855361c698c4da87bb923ff545aa02e53ddae
Marie Chirico <marie.chirico@gmail.com>
2020-11-22 18:10:11 -0500

    Adding React js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css.css';
+
 function showList(id){
     var arrow = id + '-arrow';
     var myList = document.getElementById(id);
