2896c636231ec6ecafe8323b0d0d77dcdd628073
Marie Chirico <marie.chirico@gmail.com>
2020-12-03 14:04:25 -0500

    Added portfolio thumbnails and added new button.

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './css.css';
-
 function showList(id){
     var arrow = id + '-arrow';
     var myList = document.getElementById(id);
     var myArrow = document.getElementById(arrow);
     
-    myList.style.display = "block";
     myList.className = "main-list";
 
     myArrow.className = "fas fa-angle-up";
@@ -20,6 +15,4 @@ function reset(id){
     myList.className = "fade"; 
 
     myArrow.className = "fas fa-angle-down";
-
-    setTimeout(()=> {myList.style.display = "none";}, 500); 
 }
