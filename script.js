f398cd4a2362afd528e4cdfc2c89f01c1a95ac40
Marie Chirico <marie.chirico@gmail.com>
2020-12-08 14:03:29 -0500

    Adding CSS Flex.

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 function showList(id){
-    var arrow = id + '-arrow';
     var myList = document.getElementById(id);
-    var myArrow = document.getElementById(arrow);
     
     myList.className = "main-list";
-
-    myArrow.className = "fas fa-angle-up";
 }
 function reset(id){
-    var arrow = id + '-arrow';
     var myList = document.getElementById(id);
-    var myArrow = document.getElementById(arrow);
 
     myList.className = "fade"; 
-
-    myArrow.className = "fas fa-angle-down";
 }
